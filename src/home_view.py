2fb8ec6811982ea82913f9f4f6bddbf5c81f86fc
arcsino <173187432+arcsino@users.noreply.github.com>
2026-03-21 00:32:03 +0900

    バージョン1.21.9-1.21.10, 1.21.11の追加

--- src/home_view.py
+++ src/home_view.py
@@ -1,7 +1,8 @@
-import flet as ft
 from pathlib import Path
 
-from controls import BorderImage, ExplainContainer, BodyText
+import flet as ft
+
+from controls import BodyText, BorderImage, ExplainContainer, SmallExplainContainer
 
 
 class HomeView(ft.Column):
@@ -12,10 +13,15 @@ class HomeView(ft.Column):
         self.controls = [
             ft.Divider(color=ft.Colors.TRANSPARENT),  # margin
             ExplainContainer(
-                title="Bgmrp v1.0.2",
+                title="Bgmrp v1.0.3",
                 body="MinecraftにBGMを追加するリソースパックを簡単に作れるデスクトップアプリです。",
             ),
             BodyText(value="※Java版限定です。"),
             BorderImage(src=Path("images/wp.png")),
+            ft.Divider(color=ft.Colors.TRANSPARENT),  # margin
+            SmallExplainContainer(
+                title="変更ログ",
+                body="・バージョン1.21.9-1.21.10, 1.21.11の追加",
+            ),
             ft.Divider(color=ft.Colors.TRANSPARENT),  # margin
         ]
