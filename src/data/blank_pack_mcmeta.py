04300283ad5e6b83256b0378ae7f7c3e1e370d4b
arcsino <173187432+arcsino@users.noreply.github.com>
2026-04-15 17:20:05 +0900

    フォルダ整理

--- src/data/blank_pack_mcmeta.py
+++
@@ -1,8 +0,0 @@
-def get_blank_pack_mcmeta():
-    return {
-        "pack": {
-            "pack_format": 64,
-            "supported_formats": [64, 64],
-            "description": "",
-        }
-    }
