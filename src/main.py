8db65236b72a09e97479593b02b0981159c3297d
arcsino <173187432+arcsino@users.noreply.github.com>
2026-04-24 17:35:34 +0900

    画面サイズの変更と固定

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ from views.main_view import MainView
 
 def main(page: ft.Page):
     page.title = "Bgmrp"
-    page.window.width = 720
-    page.window.height = 480
+    page.window.width = 960
+    page.window.height = 600
     page.window.resizable = False
     page.window.maximizable = False
     page.theme_mode = ft.ThemeMode.DARK
